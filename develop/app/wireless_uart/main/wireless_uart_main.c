fa0d1a8d0fe83e54352c2882fa6099e4c05ea817
hardycheng <hardycheng@msi.com>
2024-03-25 14:33:48 +0800

    * [add] wart tools * [modify] update main process for test

--- develop/app/wireless_uart/main/wireless_uart_main.c
+++ develop/app/wireless_uart/main/wireless_uart_main.c
@@ -1,34 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <stdbool.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 #include "driver/uart.h"
+#include "wuart_utils.h"
 
-#define PACKET_SYMBOL_START 0x2423
-#define PACKET_SYMBOL_END 0x2324
-#define PACKET_MIN_SIZE (2+4+2+1) //start + length + min data + checksum
-
-/*
-Packet Structure
-Start Symbol - uint16 - 0x23 0x24
-Data Length - uint32 - 4 bytes - only for data bytes, not include checksum
-Data Bytes - bytearray - [Packet Length] bytes
- > Key String - string - dynamic bytes (1~kN) - until '=', if '=' not exist, whole bytes as key string, and value as empty
- > Value Bytes - bytearray - dynamic bytes (0~vN) - until end (allow empty)
-XOR Checksum - 1 byte
-End Symbol - uint16 - 0x24 0x23 (Optional)
-*/
-typedef struct {
-    char* key_str;
-    uint8_t* val_bytes;
-    uint8_t* data_bytes;
-    uint32_t data_size;
-    uint8_t checksum;
-} Packet_t;
+static uint8_t mac_addr[6];
 
 void setup_uart(int baud_rate, int buf_size){
     // Configure parameters of an UART driver,
@@ -42,11 +22,45 @@ void setup_uart(int baud_rate, int buf_size){
     };
     uart_param_config(UART_NUM_0, &uart_config);
     // esp_err_t uart_driver_install(uart_port_t uart_num, int rx_buffer_size, int tx_buffer_size, int queue_size, QueueHandle_t *uart_queue, int no_use)
-    uart_driver_install(UART_NUM_0, buf_size * 2, 0, 0, NULL, 0);
+    // rx_buffer need to larger than UART_FIFO_LEN, tx_buffer can be zero (send data with blocking).
+    uart_driver_install(UART_NUM_0, buf_size, 0, 0, NULL, 0);
 }
 
 void app_main(){
+    if(esp_efuse_mac_get_default(mac_addr) != ESP_OK){
+        WARN("get MAC fail...");
+    }
+    setup_uart(115200, 256);
+    INFO(" ");
+    INFO("[APP] Startup..");
+    INFO("[APP] MAC Address: %02X:%02X:%02X:%02X:%02X:%02X", mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+    INFO("[APP] Free memory: %d bytes", esp_get_free_heap_size());
+    INFO("[APP] IDF version: %s", esp_get_idf_version());
     
+    Packet_t pack;
+    Packet_t* self = &pack;
+    
+    DBG("mem after create a packet: %d", esp_get_free_heap_size());
 
-    printf("Wireless UART\n");
+    char* str = packet_to_string(self);
+    DBG("before: %s", str);
+    free(str);
+
+    packet_init(self, "key1", NULL, 0);
+    str = packet_to_string(self);
+    DBG("key1:  %s", str);
+    free(str);
+
+    packet_init(self, "key2", "123", 3);
+    str = packet_to_string(self);
+    DBG("key2:  %s", str);
+    free(str);
+
+    for (int i = 999; i > 0; i--) {
+        INFO("Restarting in %d seconds...\n", i);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+    INFO("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
 }
